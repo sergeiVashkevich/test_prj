800b3897141eef5192a258a472e7f98c7cbd5153
sergejs.vaskevics <sergejs.vaskevics@ubc-713edbfc0eb.ubc.ubcmedia.com>
2010-10-11 13:44:43 +0100

    first branch changes

--- src/masterbranchpackage/FirstClass.java
+++ src/masterbranchpackage/FirstClass.java
@@ -3,4 +3,6 @@ package masterbranchpackage;
 public class FirstClass {
 		private String firstAtribute;
 		private String OnlyFirstBranchAtribute;
+		private String firstBranch;
+		private String firstBranch1;
 }
